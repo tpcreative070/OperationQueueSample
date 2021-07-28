a8449b3d864520568a25a855b33190adabec5e22
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-07-28 20:05:28 +0700

    first commit

---
+++ OperationQueueSample/Framework/OperationQueuer.xcframework/ios-arm64/OperationQueuer.framework/Headers/OperationQueuer.h
@@ -0,0 +1,18 @@
+//
+//  OperationQueuer.h
+//  OperationQueuer
+//
+//  Created by phong070 on 28/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OperationQueuer.
+FOUNDATION_EXPORT double OperationQueuerVersionNumber;
+
+//! Project version string for OperationQueuer.
+FOUNDATION_EXPORT const unsigned char OperationQueuerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OperationQueuer/PublicHeader.h>
+
+
